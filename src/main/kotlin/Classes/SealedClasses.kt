c6ae533b834cd3550c99dd96f691e0e47e8eb060
Abdulbaki <Salausmart132@gmail.com>
2025-08-30 16:30:01 +0300

    smart cast and sealed classes

---
+++ src/main/kotlin/Classes/SealedClasses.kt
@@ -0,0 +1,17 @@
+/**
+ * Reuse your solution from the previous task, but replace the interface with the sealed interface.
+ * Then you no longer need the else branch in when.
+ */
+
+package com.salausmart.Classes
+
+
+fun eval(expr: Expr): Int =
+    when (expr) {
+        is Num -> expr.value
+        is Sum -> eval(expr.left) + eval(expr.right)
+    }
+
+sealed interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
