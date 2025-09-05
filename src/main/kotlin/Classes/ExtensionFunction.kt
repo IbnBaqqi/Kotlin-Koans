5988506ed97af5ca273a202cc1242636a70df6b7
Abdulbaki <Salausmart132@gmail.com>
2025-09-05 18:54:32 +0300

    learn about extension functions

---
+++ src/main/kotlin/Classes/ExtensionFunction.kt
@@ -0,0 +1,18 @@
+/**
+ * Extension functions
+ * Learn about extension functions. Then implement the extension functions Int.r() and Pair.r() and make them convert Int and Pair to a RationalNumber.
+ * Pair is a class defined in the standard library:
+ * data class Pair<out A, out B>(
+ *     val first: A,
+ *     val second: B
+ * )
+ * In the case of Int, the denominator is 1.
+ */
+
+package com.salausmart.Classes
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
